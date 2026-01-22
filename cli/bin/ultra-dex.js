803dcbcb5759b0a315e0526ddef511d392fe237b
Srujan <roshwinram@gmail.com>
2026-01-22 21:58:27 +0530

    Bump version to 1.2.1 for CLI release

--- cli/bin/ultra-dex.js
+++ cli/bin/ultra-dex.js
@@ -109,7 +109,7 @@ Setting up the implementation plan.
 program
   .name('ultra-dex')
   .description('CLI for Ultra-Dex SaaS Implementation Framework')
-  .version('1.0.0');
+  .version('1.2.1');
 
 program
   .command('init')
